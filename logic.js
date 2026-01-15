d6e03a1b64317828e62b4f0653696d259a25e72d
Cloggy <mylord331@gmail.com>
2026-01-14 22:34:57 -0500

    initial commit and form creation with basic logic

---
+++ logic.js
@@ -0,0 +1,43 @@
+const dialog = document.getElementById('addBookDialog');
+const myLibrary = [];
+
+function Book(id, title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+    };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+function showLibrary() {
+    myLibrary.forEach(book => {
+        console.log(book.info());
+    });
+}
+
+function showDialog() {
+    dialog.showModal();
+}
+function closeDialog() {
+    dialog.close();
+}
+
+document.getElementById('addBookForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const readStatus = document.getElementById('readStatus').value;
+    const read = readStatus === 'read' ? true : false;
+
+    addBookToLibrary(title, author, pages, read);
+    closeDialog();
+    showLibrary();
+});
